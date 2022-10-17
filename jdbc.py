be7d60670257faa9e4553351f388d1bf2a274179
Lakshmikanth <94531416+k-lakshmikanth@users.noreply.github.com>
2022-10-17 23:05:01 +0530

    Update jdbc.py

--- jdbc.py
+++ jdbc.py
@@ -1,8 +1,8 @@
 #We use this in pyspark to read or write dataframe to sql database table
-jdbcUsername = "sqladmin@lksqldev1"
-jdbcPassword = "Azure$2022"
-jdbcHostname = "lksqldev1.database.windows.net"
-jdbcDatabase = "lksqldev"
+jdbcUsername = "<Username>"
+jdbcPassword = "<Password>"
+jdbcHostname = "<server_name>.database.windows.net"
+jdbcDatabase = "<Database_name>"
 jdbcPort = 1433
 jdbcUrl = "jdbc:sqlserver://{0}:{1};database={2};user={3};password={4}".format(jdbcHostname, jdbcPort, jdbcDatabase, jdbcUsername, jdbcPassword)
 
@@ -11,3 +11,10 @@ connectionProperties = {
   "password" : jdbcPassword,
   "driver" : "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 }
+
+
+# Read table from Database(jdbc) to df
+df = spark.read.jdbc(url=jdbcUrl, table="<Table_name or SQL_Query>", properties=connectionProperties)
+
+#Write df to table in Database(jdbc)
+df.write.jdbc(url=jdbcUrl, table="<Tablename with schema or default(dbo.)>", mode="overwrite", properties=connectionProperties)
